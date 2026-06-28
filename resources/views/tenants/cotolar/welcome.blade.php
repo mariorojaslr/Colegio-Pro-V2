d7493a401ae1bf8b75314ca175bb8cc59a85e340
mariorojaslr <83791643+mariorojaslr@users.noreply.github.com>
2026-06-28 18:16:31 -0300

    fix: solucionar error del acordeón removiendo el script duplicado de bootstrap que causaba conflictos

--- resources/views/tenants/cotolar/welcome.blade.php
+++ resources/views/tenants/cotolar/welcome.blade.php
@@ -878,6 +878,5 @@
     </script>
     
     <!-- Bootstrap JS for Modals -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
